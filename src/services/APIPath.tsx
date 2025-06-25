0b2af0d3eb29897096131fa1e3ba0bbf388b8984
unknown <gaelangelmelendez@gmail.com>
2025-06-24 20:32:44 -0600

    Huge-patch 1.1.2

--- src/services/APIPath.tsx
+++ src/services/APIPath.tsx
@@ -1,2 +1,2 @@
-export const APIURl = "http://192.168.1.70:5000"
-//export const APIURl = "https://auriliabackend.onrender.com" // produccion
+//export const APIURl = "http://192.168.1.70:5000"
+export const APIURl = "https://auriliabackend.onrender.com" // produccion
